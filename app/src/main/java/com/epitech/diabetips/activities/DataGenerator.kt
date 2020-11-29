921f93b02c5d4ad55fc5012761bce15f7925551d
SUC <suc.yannick@gmail.com>
2020-11-30 00:28:30 +0100

    Add basic activity

---
+++ app/src/main/java/com/epitech/diabetips/activities/DataGenerator.kt
@@ -0,0 +1,12 @@
+package com.epitech.diabetips.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.epitech.diabetips.R
+
+class DataGenerator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_generator)
+    }
+}
