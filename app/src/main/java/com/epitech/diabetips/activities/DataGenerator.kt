09bae3afe99189ab4a28d05b2a9dcf625f41c916
SUC <suc.yannick@gmail.com>
2020-11-30 01:30:09 +0100

    Add button to generate data

--- app/src/main/java/com/epitech/diabetips/activities/DataGenerator.kt
+++ app/src/main/java/com/epitech/diabetips/activities/DataGenerator.kt
@@ -3,10 +3,10 @@ package com.epitech.diabetips.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.epitech.diabetips.R
+import com.epitech.diabetips.utils.ADiabetipsActivity
 
-class DataGenerator : AppCompatActivity() {
+class DataGenerator : ADiabetipsActivity(R.layout.activity_data_generator) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_generator)
     }
 }
